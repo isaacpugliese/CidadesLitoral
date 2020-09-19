a3b173337e1b7002b8668cf7e3f343b2ab347eee
Isaac Pugliese <isaacpugliese2010gmail.com>
2020-09-18 21:04:05 -0300

    cidades finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cidadeslitoral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     *Cidades do Litoral Sul*");
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("itanhaém");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Cubatão");
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("Cananéia");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Fim! :)");
+            
+        }
+    }
+}
